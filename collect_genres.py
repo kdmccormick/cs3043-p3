5c91abd1167fe95589aeab3a271db6e3ff0b3cae
Kyle McCormick <kylemccor@gmail.com>
2018-02-23 14:59:55 -0500

    Start genre categorization work

---
+++ collect_genres.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import json
+from os import path
+import sys
+
+if path.exists('genre-map.json'):
+    if input(
+            'This will override catagorized genres.\n' + 
+            'Enter "y" to continue, or anything else to abort: '
+    ) != 'y':
+        print('Aborted.')
+        sys.exit(1)
+
+with open('user-genres.json', 'r') as f:
+    user_genres = json.loads(f.read())
+
+all_genres = []
+for genres in user_genres.values():
+    all_genres.extend(genres)
+
+with open('genre-map.json', 'w') as f:
+    f.write(
+        json.dumps(
+            {'uncategorized': sorted(list(set(all_genres)))},
+            sort_keys=True,
+            indent=4,
+            separators=(',', ': '),
+        )
+    )
+
+print('Successfully wrote to genre-map.json')
