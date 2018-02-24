5aa3377ab26fe83f5180a29fdb208ed7a4211c02
Kyle McCormick <kylemccor@gmail.com>
2018-02-24 13:27:14 -0500

    Remove duplicately categorized genres

---
+++ prioritize_genres.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import json
+
+with open('parent-genres.txt', 'r') as f:
+    parent_genre_codes = [
+        line.split(':')[0]
+        for line in f.read().splitlines()
+    ]
+
+with open('genre-map.json', 'r') as f:
+    genre_map = json.loads(f.read())
+
+already_categorized = set()
+
+for pgc in parent_genre_codes:
+    in_genre = set(genre_map[pgc])
+    genre_map[pgc] = sorted(list(in_genre - already_categorized))
+    already_categorized = already_categorized | set(genre_map[pgc])
+
+with open('genre-map.json', 'w') as f:
+    f.write(
+        json.dumps(
+            genre_map,
+            sort_keys=True,
+            indent=4,
+            separators=(',', ': '),
+        )
+    )
